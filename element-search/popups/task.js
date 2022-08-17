817dc02523b304178033ded6b10e285a367631e4
Arkadiy Stepanov <Steparrks@gmail.com>
2022-08-17 23:38:27 +0700

    update 1.1

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,13 +3,12 @@ const modalActive = document.getElementById("modal_main");
 const modalBtn = document.getElementsByClassName("show-success");
 const modalSuccess = document.getElementById("modal_success");
 
-
 modalActive.className = "modal modal_active";
 
 function close(el) {
 	el.addEventListener('click', e => {
 		let target = e.target;
-		
+
 		if(target === modalClose.item(0) || target === modalClose.item(1)) {
 			modalActive.className = "modal";
 			modalSuccess.className = "modal";
@@ -20,7 +19,7 @@ function close(el) {
 modalActive.addEventListener('click', e => {
 	let target = e.target;
 	if(target === modalBtn[0]) {
-		window.close(modalActive);
+		close(modalActive);
 		modalSuccess.className = "modal modal_active";
 	}
 });
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,40 +3,48 @@ const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 const dotArr = Array.from(document.querySelectorAll('.slider__dot'));
 
+dotArr[0].classList.add('slider__dot_active');
 
-dotArr.forEach(function(dot) {
-	dot.addEventListener('click', () => {
-	let changeIndex = dotArr.indexOf(dot);
+
+function removeOldActiveSlide() {
 	let index = imgListArr.findIndex(e => e === (imgListArr.find(item => item.classList.contains('slider__item_active'))));
 	imgListArr[index].classList.remove('slider__item_active');
-	imgListArr[changeIndex].classList.add('slider__item_active');
-
 	dotArr[index].classList.remove('slider__dot_active');
-	dotArr[changeIndex].classList.add('slider__dot_active');
-	});
-})
+
+	return index;
+}
+
+function addActiveSlide(index) {
+	imgListArr[index].classList.add('slider__item_active');
+	dotArr[index].classList.add('slider__dot_active');
+}
 
 function previouClick() {
-	let index = imgListArr.findIndex(e => e === (imgListArr.find(item => item.classList.contains('slider__item_active'))));
-	imgListArr[index].classList.remove('slider__item_active');
+	index = removeOldActiveSlide();
 	if(index === 0){
 		index = imgListArr.length;
 	}
 	index--;
-	imgListArr[index].classList.add('slider__item_active');
+	addActiveSlide(index);
 }
 
 function nextClick() {
-	let index = imgListArr.findIndex(e => e === (imgListArr.find(item => item.classList.contains('slider__item_active'))));
-	imgListArr[index].classList.remove('slider__item_active');
+	index = removeOldActiveSlide();
 	if(index === imgListArr.length - 1){
 		index = -1;
 	}
 	index++;
-	imgListArr[index].classList.add('slider__item_active');
+	addActiveSlide(index);
 	}
 
 prev.onclick = previouClick;
 
 next.onclick = nextClick;
 
+dotArr.forEach(function(dot) {
+	dot.addEventListener('click', () => {
+	let index = dotArr.indexOf(dot);
+	removeOldActiveSlide();
+	addActiveSlide(index);
+	});
+})
