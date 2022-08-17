a59d6914251417e5732006fd19759cfb44d167c8
Arkadiy Stepanov <Steparrks@gmail.com>
2022-08-17 22:12:29 +0700

    update 1.0

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,48 +2,38 @@ const imgListArr = Array.from(document.querySelectorAll('.slider__item'));
 const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 const dotArr = Array.from(document.querySelectorAll('.slider__dot'));
-let slide = 0;
 
-console.log(slide)
-function sliderDot() {
-	imgListArr
-}
 
 dotArr.forEach(function(dot) {
 	dot.addEventListener('click', () => {
-		slide = dotArr.indexOf(dot);
-		
-	});
-});
+	let changeIndex = dotArr.indexOf(dot);
+	let index = imgListArr.findIndex(e => e === (imgListArr.find(item => item.classList.contains('slider__item_active'))));
+	imgListArr[index].classList.remove('slider__item_active');
+	imgListArr[changeIndex].classList.add('slider__item_active');
 
-function previouClick(slide) {
-	for(let i = imgListArr.length - 1; i >= 0; i--) {
-		if(imgListArr[i].classList.contains('slider__item_active')) {
-			imgListArr[i].classList.remove('slider__item_active');
-			if(i === 0) {
-				i = imgListArr.length - 1;
-				imgListArr[i].classList.add('slider__item_active');
-			}else{
-				i--
-				imgListArr[i].classList.add('slider__item_active');
-			}
-		}
+	dotArr[index].classList.remove('slider__dot_active');
+	dotArr[changeIndex].classList.add('slider__dot_active');
+	});
+})
+
+function previouClick() {
+	let index = imgListArr.findIndex(e => e === (imgListArr.find(item => item.classList.contains('slider__item_active'))));
+	imgListArr[index].classList.remove('slider__item_active');
+	if(index === 0){
+		index = imgListArr.length;
 	}
+	index--;
+	imgListArr[index].classList.add('slider__item_active');
 }
 
-function nextClick(slide) {
-	for(let i = 0; i <= imgListArr.length - 1; i++) {
-		if(imgListArr[i].classList.contains('slider__item_active')) {
-			imgListArr[i].classList.remove('slider__item_active');
-			if(i === imgListArr.length - 1) {
-				i = 0;
-				imgListArr[i].classList.add('slider__item_active');
-			}else{
-				i++
-				imgListArr[i].classList.add('slider__item_active');
-			}
-		}
+function nextClick() {
+	let index = imgListArr.findIndex(e => e === (imgListArr.find(item => item.classList.contains('slider__item_active'))));
+	imgListArr[index].classList.remove('slider__item_active');
+	if(index === imgListArr.length - 1){
+		index = -1;
 	}
+	index++;
+	imgListArr[index].classList.add('slider__item_active');
 	}
 
 prev.onclick = previouClick;
