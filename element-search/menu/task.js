772f66b31decfd33cf6046c3079e091979d14ac4
Arkadiy Stepanov <Steparrks@gmail.com>
2022-08-19 00:10:18 +0700

    up 1.2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,22 +1,23 @@
-const menuSubList = document.querySelectorAll('.menu_sub')
-const menuSubListArr = Array.from(menuSubList)
+const menuLink = document.querySelectorAll('.menu_sub');
+const menuSubListArr = Array.from(menuLink);
+console.log(menuSubListArr)
 
-for (let i of menuSubListArr) {
-	const subParent = i.closest('.menu__item')
-	const subChild = subParent.querySelectorAll('.menu__link')
-	const subChildArr = Array.from(subChild)
-	for (let item of subChildArr) {
-		item.onclick = () => {
-			if(i.classList.contains('menu_active')) {
-				i.classList.remove('menu_active');
-				return false;
-			}
-			console.log(i.closest('.menu_main'))
-			if (i.closest('.menu_main').querySelector('.menu_active')) {
-				i.closest('.menu_main').querySelector('.menu_active').classList.remove('menu_active');
-			}
-			i.classList.add('menu_active');
-			return false;
-		}
-	}
+menuSubListArr.forEach((item) => {
+	item.closest('li').addEventListener('click', (event) => {
+		if(event.target === item.closest('li').querySelector('.menu__link')) {
+			event.preventDefault();
 }
+		if(item.closest('li').classList.contains('menu_active')) {
+			item.closest('li').classList.remove('menu_active');
+		}
+	console.log( menuSubListArr.find(i => i.closest('li').querySelector('.menu__link')))
+		if (item.closest('.menu_main').querySelector('.menu_active')) {
+			item.closest('.menu_main').querySelector('.menu_active').classList.remove('menu_active');
+		}
+		item.classList.add('menu_active');
+
+	})
+})
+
+
+
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,27 +1,21 @@
-const modalClose = document.getElementsByClassName("modal__close_times");
+const modalCloseArr = Array.from(document.getElementsByClassName("modal__close_times"));
 const modalActive = document.getElementById("modal_main");
 const modalBtn = document.getElementsByClassName("show-success");
 const modalSuccess = document.getElementById("modal_success");
 
 modalActive.className = "modal modal_active";
 
-function close(el) {
-	el.addEventListener('click', e => {
-		let target = e.target;
-
-		if(target === modalClose.item(0) || target === modalClose.item(1)) {
-			modalActive.className = "modal";
-			modalSuccess.className = "modal";
-		}
-	});
-}
+modalCloseArr.forEach((item) => {
+	item.addEventListener('click', () => {
+	modalActive.className = 'modal';
+	modalSuccess.className = 'modal';
+	})
+})
 
 modalActive.addEventListener('click', e => {
 	let target = e.target;
 	if(target === modalBtn[0]) {
-		close(modalActive);
+		modalActive.className = 'modal';
 		modalSuccess.className = "modal modal_active";
 	}
 });
-close(modalActive);
-close(modalSuccess);
