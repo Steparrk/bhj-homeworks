96dea3ab82704e0b583d3fecaffd3a5e66392fb4
Arkadiy Stepanov <Steparrks@gmail.com>
2022-08-19 18:21:45 +0700

    update 1.3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,21 +1,23 @@
-const menuLink = document.querySelectorAll('.menu_sub');
-const menuSubListArr = Array.from(menuLink);
-console.log(menuSubListArr)
+const menuSub = document.querySelectorAll('.menu_sub');
+const menuSubListArr = Array.from(menuSub);
+let p = null;
 
 menuSubListArr.forEach((item) => {
 	item.closest('li').addEventListener('click', (event) => {
 		if(event.target === item.closest('li').querySelector('.menu__link')) {
 			event.preventDefault();
-}
-		if(item.closest('li').classList.contains('menu_active')) {
-			item.closest('li').classList.remove('menu_active');
 		}
-	console.log( menuSubListArr.find(i => i.closest('li').querySelector('.menu__link')))
-		if (item.closest('.menu_main').querySelector('.menu_active')) {
-			item.closest('.menu_main').querySelector('.menu_active').classList.remove('menu_active');
+		if(p !== item.closest('li').querySelector('.menu__link')) {
+			menuSubListArr.filter(el => el.classList.remove('menu_active'));
+			p = item.closest('li').querySelector('.menu__link');
+			item.classList.add('menu_active');
+		}else{
+			if(item.classList.contains('menu_active')){
+				menuSubListArr.filter(el => el.classList.remove('menu_active'));
+			}else{
+				item.classList.add('menu_active');
+			}
 		}
-		item.classList.add('menu_active');
-
 	})
 })
 
