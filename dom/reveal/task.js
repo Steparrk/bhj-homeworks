d60ef13d4e9f3e301170010cccc3c1442dab8708
Arkadiy Stepanov <Steparrks@gmail.com>
2022-09-01 21:32:32 +0700

    up 1.1

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,15 +1,18 @@
-const reveal = Array.from(document.getElementsByClassName('reveal'));
+const reveal = Array.from(document.querySelectorAll('.reveal'));
 
 document.addEventListener('scroll', function() {
     reveal.forEach(item => {
         if (isInViewarea(item)){
-        item.classList.add('reveal_active');
+            item.classList.remove('reveal_active');
+            return;
         }
+        item.classList.add('reveal_active');
     });
 });
 
 const isInViewarea = function(element) {
     const viewportHeight = window.innerHeight;
     const elementTop = element.getBoundingClientRect().top;
-    return elementTop < viewportHeight ? true : false;
+    const elementBottom = element.getBoundingClientRect().bottom;
+    return (elementTop > viewportHeight || elementBottom < 0) ? true : false;
 };
